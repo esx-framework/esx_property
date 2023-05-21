fe33f9bede8f2262aa68498008d7298fce6b0bf8
TheFantomas <117121911+TheFantomas@users.noreply.github.com>
2023-05-21 10:35:27 +0200

    📖

--- modules/cctv.lua
+++ modules/cctv.lua
@@ -4,7 +4,7 @@ Core.Modules.CCTV = {}
 --- @param propertyId integer
 function Core.Modules.CCTV:OpenCCTV(propertyId)
     local property = Core.Properties[propertyId]
-    assert(property, 'Property '..propertyId..' not found')
+    assert(property, 'Property ' .. propertyId .. ' not found')
     if not property.cctv.enabled then return ESX.ShowNotification('CCTV is turned off!') end
     DoScreenFadeOut(500)
     while IsScreenFadingIn() do
@@ -50,46 +50,46 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
             HideHudComponentThisFrame(6)
             HideHudComponentThisFrame(19)
             HideHudAndRadarThisFrame()
-  
+
             local instructions = Core.Utils:CreateInstuctionScaleform('instructional_buttons')
             DrawScaleformMovieFullscreen(instructions, 255, 255, 255, 255, 0)
 
             local getCameraRot = GetCamRot(cctvCamera, 2)
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Left) and getCameraRot.z < property.cctv.maxleft then
-              PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
-              SetCamRot(cctvCamera, getCameraRot.x, 0.0, getCameraRot.z + Config.CCTV.RotateSpeed, 2)
+                PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
+                SetCamRot(cctvCamera, getCameraRot.x, 0.0, getCameraRot.z + Config.CCTV.RotateSpeed, 2)
             end
             -- ROTATE RIGHT
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Right) and getCameraRot.z > property.cctv.maxright then
-              PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
-              SetCamRot(cctvCamera, getCameraRot.x, 0.0, getCameraRot.z - Config.CCTV.RotateSpeed, 2)
+                PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
+                SetCamRot(cctvCamera, getCameraRot.x, 0.0, getCameraRot.z - Config.CCTV.RotateSpeed, 2)
             end
-  
+
             -- ROTATE UP
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Up) and getCameraRot.x < Config.CCTV.MaxUpRotation then
-              PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
-              SetCamRot(cctvCamera, getCameraRot.x + Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
+                PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
+                SetCamRot(cctvCamera, getCameraRot.x + Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
             end
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Down) and getCameraRot.x > Config.CCTV.MaxDownRotation then
-              PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
-              SetCamRot(cctvCamera, getCameraRot.x - Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
+                PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
+                SetCamRot(cctvCamera, getCameraRot.x - Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
             end
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.ZoomIn) and GetCamFov(cctvCamera) > Config.CCTV.MaxZoom then
-              SetCamFov(cctvCamera, GetCamFov(cctvCamera) - 1.0)
+                SetCamFov(cctvCamera, GetCamFov(cctvCamera) - 1.0)
             end
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.ZoomOut) and GetCamFov(cctvCamera) < Config.CCTV.MinZoom then
-              SetCamFov(cctvCamera, GetCamFov(cctvCamera) + 1.0)
+                SetCamFov(cctvCamera, GetCamFov(cctvCamera) + 1.0)
             end
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Down) and getCameraRot.x > Config.CCTV.MaxDownRotation then
-              PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
-              SetCamRot(cctvCamera, getCameraRot.x - Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
+                PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
+                SetCamRot(cctvCamera, getCameraRot.x - Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
             end
-  
+
             SetTextFont(4)
             SetTextScale(0.8, 0.8)
             SetTextColour(255, 255, 255, 255)
@@ -97,7 +97,7 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
             BeginTextCommandDisplayText('STRING')
             AddTextComponentSubstringPlayerName(property.setName ~= "" and property.setName or property.Name)
             EndTextCommandDisplayText(0.01, 0.01)
-  
+
             SetTextFont(4)
             SetTextScale(0.7, 0.7)
             SetTextColour(255, 255, 255, 255)
@@ -106,7 +106,7 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
             local yr, mo, da, hr, min, sec = GetPosixTime()
             AddTextComponentSubstringPlayerName("" .. da .. "/" .. mo .. "/" .. yr .. " " .. hr .. ":" .. min .. ":" .. sec)
             EndTextCommandDisplayText(0.01, 0.055)
-  
+
             SetTextFont(4)
             SetTextScale(0.6, 0.6)
             SetTextColour(255, 255, 255, 255)
@@ -115,7 +115,7 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
             local zoom = ((Config.CCTV.FOV - GetCamFov(cctvCamera)) / GetCamFov(cctvCamera)) * 100
             AddTextComponentSubstringPlayerName(TranslateCap("zoom_level", math.floor(zoom)))
             EndTextCommandDisplayText(0.01, 0.09)
-  
+
             SetTextFont(4)
             SetTextScale(0.6, 0.6)
             SetTextColour(255, 255, 255, 255)
@@ -123,7 +123,7 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
             BeginTextCommandDisplayText('STRING')
             AddTextComponentSubstringPlayerName(nightVision and "Night Vision: Active" or "CCTV System: Active")
             EndTextCommandDisplayText(0.01, 0.12)
-  
+
             if IsDisabledControlPressed(0, Config.CCTV.Controls.Down) and getCameraRot.x > Config.CCTV.MaxDownRotation then
                 PlaySoundFrontend(-1, "FocusIn", "HintCamSounds", false)
                 SetCamRot(cctvCamera, getCameraRot.x - Config.CCTV.RotateSpeed, 0.0, getCameraRot.z, 2)
@@ -134,26 +134,26 @@ function Core.Modules.CCTV:OpenCCTV(propertyId)
                 SetNightvision(nightVision)
                 SetTimecycleModifier("scanline_cam")
             end
-  
+
             if Config.CCTV.PictureWebook ~= "" and IsDisabledControlJustPressed(0, 201) and not pictureCooldown then
                 Wait(1)
                 PlaySoundFrontend(-1, "Camera_Shoot", "Phone_Soundset_Franklin", 1)
                 ESX.TriggerServerCallback("esx_property:GetWebhook", function(hook)
-                  if hook then
-                    exports['screenshot-basic']:requestScreenshotUpload(hook, "files[]", function(data)
-                      local image = json.decode(data)
-                      ESX.ShowNotification(TranslateCap("picture_taken"), "success")
-                      SendNUIMessage({link = image.attachments[1].proxy_url})
-                      ESX.ShowNotification(TranslateCap("clipboard"), "success")
-                      pictureCooldown = true
-                      SetTimeout(5000, function()
-                        pictureCooldown = false
-                      end)
-                    end)
-                  end
+                    if hook then
+                        exports['screenshot-basic']:requestScreenshotUpload(hook, "files[]", function(data)
+                            local image = json.decode(data)
+                            ESX.ShowNotification(TranslateCap("picture_taken"), "success")
+                            SendNUIMessage({ link = image.attachments[1].proxy_url })
+                            ESX.ShowNotification(TranslateCap("clipboard"), "success")
+                            pictureCooldown = true
+                            SetTimeout(5000, function()
+                                pictureCooldown = false
+                            end)
+                        end)
+                    end
                 end)
             end
-  
+
             if IsDisabledControlPressed(1, Config.CCTV.Controls.Exit) then
                 DoScreenFadeOut(1000)
                 ESX.TriggerServerCallback("esx_property:ExitCCTV", function(CanExit)
